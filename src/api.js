00979cd2d183e61706fd34b4fad0c5fb66baf867
Shivam Nikam <shivammh15@gmail.com>
2023-02-23 15:01:22 +0530

    React Weather App

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'f247eb7a6amsh5913d428ca23a8fp15f0c0jsnd2cd6c57120a',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "4c5ef5601d72782493b1a16b6ac0aa01";
